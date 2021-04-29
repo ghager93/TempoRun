8b388c3c872700d1e6a4228c0c98119b99073ae7
ghage <ghager93@gmail.com>
2021-04-29 13:05:39 +1000

    moving average

--- peak_detection.py
+++ peak_detection.py
@@ -3,6 +3,10 @@ import numpy as np
 from scipy import ndimage
 
 
+def moving_average_filter(arr, window_length):
+    return np.convolve(arr, np.ones(window_length)/window_length, mode='same')
+
+
 def max_filter(arr, window_length):
     maxs = ndimage.maximum_filter1d(arr, window_length, mode='constant')
     return np.where(arr == maxs, arr, 0)
