2cb2c6315a93d7dc961e12315312b476e786d5ff
ghage <ghager93@gmail.com>
2021-05-05 16:30:00 +1000

    renamed ampd to ampd_peak_detection.py peak_labelling.py origin

--- peak_labelling.py
+++ peak_labelling.py
@@ -1,6 +1,14 @@
 import numpy as np
 
-import ampd
+import ampd_peak_detection
 import filters
 
 
+def ampd(arr, fs):
+    return ampd_peak_detection.find_peaks(arr, lag_max=int(min(fs, len(arr)//2)), lag_min=1)
+
+
+def max_threshold(arr, fs):
+    arr_filtered = filters.max_threshold(arr, int(max(fs//9, 4)))
+
+    return arr_filtered == 0
