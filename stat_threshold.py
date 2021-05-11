eb30fd9bfa542a3ad68c20a8df49b8501b519046
ghage <ghager93@gmail.com>
2021-05-11 14:02:54 +1000

    much faster mad method by ajcr

--- stat_threshold.py
+++ stat_threshold.py
@@ -1,7 +1,12 @@
 import numpy as np
+import rolling
 
 
 def rolling_median_absolute_deviation(arr, window_length):
+    return rolling_median_absolute_deviation_by_skip_list(arr, window_length)
+
+
+def rolling_median_absolute_deviation_by_numpy(arr, window_length):
     if window_length % 2:
         mid = int((window_length - 1) / 2)
         def median_mid_func(x): return x[mid]
@@ -27,5 +32,27 @@ def rolling_median_absolute_deviation(arr, window_length):
     medians[-1] = median_mid_func(s)
     t = np.sort(abs(s - medians[i]))
     mads[-1] = median_mid_func(t)
+
+    return mads, medians
+
+def rolling_median_absolute_deviation_by_skip_list(arr, window_length):
+    # Using rolling function from https://github.com/ajcr/rolling
+    # Which uses skip_list method from Raymond Hettinger - https://code.activestate.com/recipes/577073/
+
+    mid = int(window_length / 2)
+    arr_med = np.median(arr)
+    arr_pad = np.concatenate((arr_med * np.ones(mid), arr, arr_med * np.ones(mid)), axis=None)
+
+    medians = np.array(list(rolling.Median(arr, window_length)))
+
+    if window_length % 2 == 0:
+        medians = medians[:-1]
+
+    median_med = np.median(medians)
+    median_pad = np.concatenate((median_med * np.ones(mid), medians, median_med * np.ones(mid)), axis=None)
+
+    mads = np.array(list(rolling.Median(median_pad, window_length)))
+    if window_length % 2 == 0:
+        mads = mads[:-1]
 
     return mads, medians
