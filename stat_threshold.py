950bd3ae86651a29a2f8b0af10640914e67d99fe
ghage <ghager93@gmail.com>
2021-05-11 12:17:33 +1000

    rolling MAD doesnt woooooork

--- stat_threshold.py
+++ stat_threshold.py
@@ -7,13 +7,15 @@ def rolling_median_absolute_deviation(arr, window_length):
         def median_mid_func(x): return x[mid]
     else:
         mid = int(window_length / 2)
-        def median_mid_func(x, mid): return 0.5*(x[mid-1] + x[mid])
+        def median_mid_func(x): return 0.5*(x[mid-1] + x[mid])
 
     arr_med = np.median(arr)
     medians = np.zeros(len(arr))
     mads = np.zeros(len(arr))
 
     arr_pad = np.concatenate((arr_med * np.ones(mid), arr, arr_med * np.ones(mid)), axis=None)
+    print(arr_pad)
+
 
     s = np.sort(arr_pad[:window_length])
     for i in range(mid, len(arr) - mid + 1):
@@ -23,5 +25,7 @@ def rolling_median_absolute_deviation(arr, window_length):
         mads[i - mid] = median_mid_func(t)
 
         s = np.sort(np.append(s[1:], arr_pad[i]))
+
+    print(arr_pad)
 
     return mads, medians
